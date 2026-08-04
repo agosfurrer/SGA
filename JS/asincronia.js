bd834772913706336ddba2ee6aa1c6d3c5b32ba8
AgosF <agosfurrer06@gmail.com>
2026-08-04 14:58:22 -0300

    Clase 02 - Agrego estructura js y primer ejemplo de asincronía

---
+++ JS/asincronia.js
@@ -0,0 +1,39 @@
+
+// console.log("Inicio")
+// setTimeout(() => {
+//     console.log("Buscando alumno...")
+// }, 3000) //3seg espera de resp
+// console.log("Fin")
+// // -----------------
+
+// function saludar() {
+//     console.log("Hola")
+// }
+// function ejecutar(funcion) {
+//     funcion();
+// }
+// ejecutar(saludar)//callback
+
+
+// function despedirse() {
+//     console.log("Hasta luego")
+// }
+// setTimeout(despedirse, 3000)//callback
+// //esperas
+// setTimeout(() => {
+//     console.log("Buscando docentes")
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Buscando materias")
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log("Buscando cursos")
+// }, 1000);
+
+console.log("Abriendo SGA")
+setTimeout(() => {
+    console.log("Alumnos cargados")
+}, 3000);
+console.log("Usuario puede seguir")
