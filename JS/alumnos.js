81612753c97b5362c3bf853b066ab076cb2a2486
AgosF <agosfurrer06@gmail.com>
2026-08-24 15:48:37 -0300

    Clase 11 - Cambio visual y comienzo refactorización

--- JS/alumnos.js
+++ JS/alumnos.js
@@ -222,12 +222,12 @@ function obtenerAlumno () {
     return [] //evita q devuelva nulo
 }
 
-function mostrarMensaje(texto, tipo) {
+function mostrarMensaje(texto, clase) {
     mensaje.textContent = texto;
-    mensaje.className = tipo
+    mensaje.className = `mensaje ${clase}`
+    mensaje.style.display = "block"
     setTimeout(() => {
-        mensaje.textContent = " ";
-        mensaje.className = "oculto"
+        mensaje.style.display = "none"
     }, 3000);
 }
 
